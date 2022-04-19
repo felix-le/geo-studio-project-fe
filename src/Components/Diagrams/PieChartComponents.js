2a28e27992a45b136b3ec0bc02b4e6f4727b3485
Tanya Pachkalov <78969794+Tanyap1@users.noreply.github.com>
2022-04-19 08:02:03 -0400

    Update PieChartComponents.js

--- src/Components/Diagrams/PieChartComponents.js
+++ src/Components/Diagrams/PieChartComponents.js
@@ -23,7 +23,7 @@ const useStyles = makeStyles((theme) => ({
     '@media (max-width: 1000px)': {
       flexWrap: 'wrap',
     },
-    fontFamily:' "Tajawal", "sans-serif",Arial, Helvetica, sans-serif',
+    fontFamily: 'Tajawal', 'sans-serif',
   },
   titlePercentage: {
     fontSize: '1rem',
